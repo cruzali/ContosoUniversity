fdc6ceee864fe61162e587f154b0e839b9924fd3
Ali Cruz <ali.cruz@me.com>
2021-12-04 00:49:57 -0600

    Primer commit

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    // Entidad student, en el contexto de base de datos.
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // Si una propiedad de navegación puede contener varias entidades
+        // entonces resulta en un listado y debe usar: 
+        // ICollection, List o HashSet
+        // Una propiedad de navegación contiene todas las entidades de relación
+        // 
+        public ICollection<Enrollment> Enrollments { get; set; }
+        // ICollection crea un HashSet<T> de forma predeterminada
+
+    }
+}
